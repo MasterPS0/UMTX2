d5945a9d907091f4f8ad606a58ec8ae5a5b91dc1
Master <49209220+Master-s@users.noreply.github.com>
2025-09-09 02:56:48 +0400

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -37,7 +37,7 @@ const payload_map = [
         author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
         projectSource: "https://github.com/etaHEN/etaHEN",
         binarySource: "https://github.com/etaHEN/etaHEN/blob/360e7c0d2dddf4f4645599d51d652e707c0b717b/etaHEN-2.0b.bin",
-        version: "2.0b",
+        version: "2.3B",
         toPort: 9021
     },
 	{
