569e5d3fc1ba01876ad6b5c10946530a25b9389b
MasterPS0 <49209220+Master-s@users.noreply.github.com>
2026-09-20 23:59:44 +0400

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -103,13 +103,13 @@ const payload_map = [
         toPort: 9021
     },
     {
-        displayTitle: "Kstuff Lite 1.12t",
+        displayTitle: "Kstuff Lite 1.11",
         description: "FPKG enabler",
         fileName: "kstuff.elf",
         author: "sleirsgoevy, john-tornblom, EchoStretch, buzzer-re, BestPig, LightningMods, zecoxao, idlesauce",
         projectSource: "https://github.com/EchoStretch/kstuff-lite",
         binarySource: "https://github.com/EchoStretch/kstuff-lite/releases/download/v1.01/",
-        version: "1.12",
+        version: "1.11",
         toPort: 9021
     },
     {
